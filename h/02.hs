e10c411cbe74c1c35060594222767fbe4b6824c1
Matt Audesse <matt@mattaudesse.com>
2015-05-31 13:39:44 -0400

    Add solution to problem #02.

---
+++ h/02.hs
@@ -0,0 +1,30 @@
+{-# ANN module "HLint: ignore Use string literal" #-}
+
+{-
+    Problem 2
+    Find the last but one element of a list.
+
+    Example in Haskell:
+
+    Prelude> myButLast [1,2,3,4]
+    3
+
+    Prelude> myButLast ['a'..'z']
+    'y'
+-}
+
+myButLast :: [a] -> a
+myButLast = head . drop 1 . reverse 
+
+
+main :: IO ()
+main = do
+    putStrLn "myButLast [1,2,3,4]: "
+    -- HLint complains with a type constraint warning unless we add 
+    -- the ":: Integer" type annotation here:
+    print (myButLast [1,2,3,4] :: Integer)
+
+    putStrLn "myButLast ['x','y','z']: "
+    -- Likewise, HLint wants an array of Char to be expressed as
+    -- a string instead, hence the ignore pragma at top:
+    print (myButLast ['x','y','z'])
