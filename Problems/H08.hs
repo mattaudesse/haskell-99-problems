43e62ee18e2eb0c7258ba2adfb2a8f9081e0a6ec
Matt Audesse <matt@mattaudesse.com>
2015-06-26 15:56:11 -0400

    Add a nicer solution for problem #08.

--- Problems/H08.hs
+++ Problems/H08.hs
@@ -1,5 +1,7 @@
 module Problems.H08 where
 
+import Data.List (group)
+
 
 -- |
 -- Eliminate consecutive duplicates of list elements.
@@ -18,3 +20,19 @@ compress xs = foldr compress' [] xs where
     compress' x (a:as) 
         | x == a            = a:as
         | otherwise         = x:a:as 
+
+
+-- |
+-- (A better solution from: https://wiki.haskell.org/99_questions/Solutions/8)
+--
+-- Eliminate consecutive duplicates of list elements.
+--
+-- If a list contains repeated elements they should be replaced with a single
+-- copy of the element. The order of the elements should not be changed.
+--
+-- >>> compress'' "aaaabccaadeeee"
+-- "abcade"
+
+
+compress'' :: Eq a => [a] -> [a]
+compress'' = map head . group
