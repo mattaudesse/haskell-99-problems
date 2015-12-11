fbcedfb138b6f873fe08e02528dd6ae0fa32576b
Matt Audesse <matt@mattaudesse.com>
2015-12-11 14:50:44 -0500

    Replace explicit doctest file list with containing directory.

--- tests/doctest.hs
+++ tests/doctest.hs
@@ -3,17 +3,4 @@ module Main where
 import Test.DocTest
 
 main :: IO ()
-main = doctest [ 
-                 "Problems/H01.hs"
-               , "Problems/H02.hs"
-               , "Problems/H03.hs"
-               , "Problems/H04.hs"
-               , "Problems/H05.hs"
-               , "Problems/H06.hs"
-               , "Problems/H07.hs"
-               , "Problems/H08.hs"
-               , "Problems/H09.hs"
-               , "Problems/H10.hs"
-               , "Problems/H11.hs"
-               , "Problems/H12.hs"
-               ]
+main = doctest ["Problems"]
